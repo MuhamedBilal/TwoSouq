d42cd190647997b5fb4ab500f7bc94d6066a9350
MuhamedBilal <mohamedbelal109@gmail.com>
2024-01-10 10:27:09 +0200

    feat:Categories section, done

---
+++ src/assets/icons.js
@@ -0,0 +1,29 @@
+import estate from "./estate.jpg";
+import phones from "./phones.jpg";
+import electronics from "./electronics.jpg";
+import pets from "./pets.jpg";
+import manufactures from "./manufacturing.jpg";
+import cars from "./cars.jpg";
+import others from "./others.jpg";
+import decor from "./decor.jpg";
+import fashion from "./fashion.jpg";
+import sports from "./sports.jpg";
+import services from "./services.jpg";
+import jobs from "./jobs.jpg";
+
+const icons = [
+  { image: estate, description: "عقارات" },
+  { image: phones, description: "موبايلات و تابلت" },
+  { image: electronics, description: "الكترونيات" },
+  { image: pets, description: "حيوانات أليفة" },
+  { image: manufactures, description: "الأعمال و الصناعه" },
+  { image: cars, description: "مركبات و سيارات" },
+  { image: others, description: "أخري" },
+  { image: decor, description: "اثاث و ديكور" },
+  { image: fashion, description: "موضة و مسلتزمات تجميل" },
+  { image: sports, description: "المستلزمات الرياضية و الهويات" },
+  { image: services, description: "خدمات" },
+  { image: jobs, description: "وظائف" },
+];
+
+export default icons;
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../styles/Categories.css'
+import icons from '../assets/icons.js'
 
 function Categories() {
   return (
-    <div>Categories</div>
+    <div className='categories'>
+
+    <div className='heading'>الفقات</div>
+
+    <div className='grid-container'>
+        {icons.map((item, index) => (
+          <button key={index} className='grid-item'>
+            <img src={item.image} alt={item.description} className='category-image' />
+            <div className='category-description'>{item.description}</div>
+          </button>
+        ))}
+      </div>
+    </div>
   )
 }
 
